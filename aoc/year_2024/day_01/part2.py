e738259fa5cdb1bd8c2f2f0126c83d831261d780
Pierre-Yves PAMART <pypamart@gmail.com>
2024-12-12 14:38:09 +0100

    ♻️ Optimize solution for Part II by using Counter for improved performance

--- aoc/year_2024/day_01/part2.py
+++ aoc/year_2024/day_01/part2.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+from collections import Counter
+
 from aoc.year_2024.day_01.input_reader import read_lists
 
 INPUT_FILE = Path(__file__).parent / "resources" / "lists.txt"
@@ -7,10 +9,9 @@ INPUT_FILE = Path(__file__).parent / "resources" / "lists.txt"
 
 def solve(filepath: Path=INPUT_FILE.absolute()) -> int:
     list_1, list_2 = read_lists(filepath)
+        
+    counter_list_2 = Counter(list_2)
+    similarity = sum(value * counter_list_2[value] for value in list_1)
 
-    similarity = 0
-    for value in list_1:
-        similarity += value * list_2.count(value)
-    
     print("Part II solution: ", similarity)
     return similarity
